07269d899c115d288ca99a4826e8e8c0b57d8525
ShmuelRozental <rozentalshmuel@gmail.com>
2024-08-21 10:54:18 +0300

    Initial commit for Real-Time Mossad Agents Management System

---
+++ Models/Agent.cs
@@ -0,0 +1,11 @@
+﻿namespace Real_Time_Mossad_Agents_Management_System.Models
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+        public string NikName { get; set; }
+        public PinLocation PinLocation { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
---
+++ Models/PinLocation.cs
@@ -0,0 +1,8 @@
+﻿namespace Real_Time_Mossad_Agents_Management_System.Models
+{
+    public class PinLocation
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
